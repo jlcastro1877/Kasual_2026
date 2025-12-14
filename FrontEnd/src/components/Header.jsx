aef32b31805c5eae3f5c3eea9e87a2e4dcac2c4c
Jorge Luiz de Castro Junior <jlcastro@churchofjesuschrist.org>
2025-12-13 21:18:04 -0700

    tentativa de por logo

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Kasual Tricot 2026</Navbar.Brand>
+          <Navbar.Brand href="/">
+            {/* <img src={logo} alt="Kasual Tricot 2026" /> */}
+            Kasual Tricot 2026
+          </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
